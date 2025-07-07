4d0b091e02b42ecad2c3a119068319543eb7db91
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-07-07 22:25:39 +0530

    fibonacci series.

--- recursion/fibonaci.cpp
+++ recursion/fibonaci.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int fibonaci(int n){
-    if(n<=1){
+int fibonaci(int n)
+{
+    if (n <= 1)
+    {
         return n;
     }
-    return fibonaci(n-1)+fibonaci(n-2);
+    return fibonaci(n - 1) + fibonaci(n - 2);
 }
-int main(){
+int main()
+{
     int n;
-    cout<<"Enter the number from which you want the series : ";
-    cin>>n;
-    cout<<fibonaci(n);
+    cout << "Enter the number from which you want the series : ";
+    cin >> n;
+    cout << fibonaci(n);
 }
