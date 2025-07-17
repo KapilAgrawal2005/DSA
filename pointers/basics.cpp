fa22b5d7bf96d687835b460b30aa16122313abe3
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-07-17 23:48:36 +0530

    Started the pointers

---
+++ pointers/basics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 5;
+    cout << "The number is : " << n << endl;
+
+    int &num = n;
+    cout << "The address of the n is : " << &num << endl;
+
+    int *ptr = &num;
+    *ptr += 1;
+    cout << "The value of num is : " << num << endl
+         << "The value of ptr is : " << *ptr;
+}
