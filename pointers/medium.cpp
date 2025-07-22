cb26f8b26b35f47643895b78374395ad0bdc7dce
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-07-22 16:39:52 +0530

    pointers

--- pointers/medium.cpp
+++ pointers/medium.cpp
@@ -2,9 +2,15 @@
 using namespace std;
 int main()
 {
-    int arr[10] = {0};
+    int arr[10] = {3,6,3};
     
     cout<<"The address of the first element of the array is : "<<arr<<endl;
     
     cout<<"The address of the first element of the array is : "<<&arr[0]<<endl;
+
+    cout<< *arr<<endl;
+    cout<<*(arr + 6)<<endl;
+
+    // formula => arr[i] == *(arr + i);
+    // formula => i[arr] == *(i + arr);
 }
