a6eb099e972a11f047bd436df1c65493a5713cd4
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-07-27 14:45:13 +0530

    Fist and last occurance

--- binarySearch/firstAndLast.cpp
+++ binarySearch/firstAndLast.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int binarySearch(int a[], int n, int k)
+void firstAndLastOccurance(int a[], int n, int k)
 {
     int start = 0;
     int end = n - 1;
-    int mid;
+    int mid, first = -1, last = -1;
     while (start <= end)
     {
         mid = start + (end - start) / 2;
         if (a[mid] == k)
         {
-            return mid;
+            first = mid;
+            end = mid - 1;
         }
         else if (a[mid] > k)
         {
@@ -21,7 +22,29 @@ int binarySearch(int a[], int n, int k)
             start = mid + 1;
         }
     }
-    return -1;
+
+    start = 0;
+    end = n - 1;
+    while (start <= end)
+    {
+        mid = start + (end - start) / 2;
+        if (a[mid] == k)
+        {
+            last = mid;
+            start = mid + 1;
+        }
+        else if (a[mid] > k)
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+
+    cout << first << " ";
+    cout << last;
 }
 
 int main()
@@ -39,5 +62,5 @@ int main()
     cout << "Enter the number that you want to search : ";
     cin >> k;
 
-    cout << "The elements has been found at index at : " << binarySearch(a, n, k);
+    firstAndLastOccurance(a, n, k);
 }
