03fbd1b1b2fc1626cbaf7eef318a68ff5f2e107b
KapilAgrawal2005 <kapilagrawal448@gmail.com>
2025-08-04 15:23:42 +0530

    String sorting

--- strings/sorting.cpp
+++ strings/sorting.cpp
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-string sorting(string &s) {
-    int a[26]={0};
+string sorting(string &s)
+{
+    int a[26] = {0};
     string temp = "";
     int count = 0, c = 0;
-    for(int i =0; i<s.size();i++){
-        a[s[i] - 'a']+=1;
+    for (int i = 0; i < s.size(); i++)
+    {
+        a[s[i] - 'a'] += 1;
     }
-    for (int i = 0; i<26;i++){
-        while(a[i]){
-            temp += 'a' +i;
+    for (int i = 0; i < 26; i++)
+    {
+        while (a[i])
+        {
+            temp += 'a' + i;
             a[i]--;
         }
     }
@@ -18,8 +22,8 @@ string sorting(string &s) {
 int main()
 {
     string s;
-    cout<<"enter the strings.";
-    getline(cin,s);
-    cout<<sorting(s);
+    cout << "enter the strings.";
+    getline(cin, s);
+    cout << sorting(s);
     return 0;
 }
